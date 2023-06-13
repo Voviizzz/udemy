447e0446e2c8fad186d4ab2d9149b22a4a17f3f5
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-06-13 13:24:57 +0300

    Введение в колбек

---
+++ callback/callback.js
@@ -0,0 +1,30 @@
+// function first() {
+//   // do something
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 699);
+// }
+// function second() {
+//   console.log(2);
+// }
+// function learnJs(lang, callback) {
+//   console.log(`я учу ${lang}`);
+//   callback();
+// }
+// learnJs('java', function(){
+//     console.log('я прошел урок');
+// })
+
+// learnJs("js", done);
+// function done() {
+//   console.log("я прошел курс");
+// }
+
+function hellUser(name, callback) {
+  console.log(`hello ${name}`);
+  callback();
+}
+function callback() {
+  console.log(`мы поздоровались с юзером`);
+}
+hellUser('Джон', callback)
