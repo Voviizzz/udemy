d871a918aef0dc98e404edb4766e956afee69260
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-06-21 10:22:44 +0300

    Тпизация

---
+++ dynamic_tipization/dynamic_tipization.js
@@ -0,0 +1,35 @@
+console.log(typeof String(null));
+
+// 2)
+console.log(typeof (6 + ""));
+
+const num = 5;
+console.log("https://vk.com/catalog/" + num);
+
+const fS = 26 + "px";
+
+//to number
+
+// 1)console.log(typeof(Number('sd')));
+
+console.log(typeof +"5");
+
+// 3)
+console.log(typeof parseInt("15px", 10));
+
+// let answ = +prompt('hello', '')
+
+// to Boolean
+
+// 0, '', null, undefined, NaN; - false
+
+// Все остальное тру
+// 1)
+let switcer = null;
+switcer = 1;
+
+if (switcer) {
+  console.log("working...");
+}
+
+// 2)
