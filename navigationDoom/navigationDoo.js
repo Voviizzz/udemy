98ee4b3bafdadf101060bc983bfe4c308651a2d2
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-07-03 13:56:01 +0300

    Используем собития на странице проекта

---
+++ navigationDoom/navigationDoo.js
@@ -0,0 +1,13 @@
+// console.log(document.body);
+
+// console.log(document.body.childNodes); // для получения дочерних элементов \n - это текстовая нода !
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector("#current").parentNode.parentNode);
+
+// ____________________________________//ДАТА АТРИБУТЫ!
+
+// console.log(document.querySelector('[data-current="3"').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"').previousElementSibling);
