8b8e33e5e22dfff86cbb993524fea80b2fe2f924
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-07-18 16:49:47 +0300

    JS в работе, проект FOOD

---
+++ javaScript_В_Работе/tabs/js/copyMain.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", () => {
+  //Разбиваем на константы
+  //вся коллекция кнопок хранится в btns
+  //   --
+  const btns = document.querySelectorAll(".tabheader__item");
+  //   console.log(btns);
+  //   --
+  //вся коллекция контента хранитя в tabcontent
+  const tabContent = document.querySelectorAll(".tabcontent");
+  //   console.log(tabcontent);
+  //   --
+  //родитель нам нужен для делегирования
+  const btnParrent = document.querySelector(".tabheader__items");
+  // Функция для скрытия контента
+  function hideTabContent() {
+    tabcontent.forEach((item) => {
+      item.style.display = "none";
+    });
+    //убираем активный класс у кнопок и ставим NONE контенту
+    btns.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  //по умолчанию у нас отображается первый контент из коллекции контента и кнопки
+  function showTabContent(i = 0) {
+    tabcontent[i].style.display = "block";
+    btns[i].classList.add("tabheader__item_active");
+  }
+  //вызываем обе функции
+  hideTabContent();
+  showTabContent();
+
+  //Делегирование событий, чтобы при нажатии на таб - менялся контент
+  btnParrent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      btns.forEach((btn, i) => {
+        if (target == btn) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
